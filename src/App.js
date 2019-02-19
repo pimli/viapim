b2159cef3906d24333f7e2d79104c1a77d466202
Pim Lindahl <pixelpim@hotmail.com>
2019-02-19 21:19:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Yes
+      </div>
+    );
+  }
+}
+
+export default App;
